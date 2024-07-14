3894b54c5654ab08450a3fcb43540cf4a8f87e85
Ceci Ivanov <cceeci99@gmail.com>
2024-07-14 17:13:56 +0300

    update readme

--- scripts/examples.sh
+++ scripts/examples.sh
@@ -1,13 +1,13 @@
-# GET valid user favorites
+# GET existing user favorites
 curl -X GET http://localhost:8080/users/1/favorites
 
-# GET invalid user favorites
+# GET user favorites which do not exist
 curl -X GET http://localhost:8080/users/999999/favorites
 
-# DELETE valid user favorite
+# DELETE existing user favorite
 curl -X DELETE http://localhost:8080/users/1/favorites/1
 
-# DELETE invalid user favorite
+# DELETE user favorite which does not exist
 curl -X DELETE http://localhost:8080/users/1/favorites/999999
 
 # ADD valid user favorite
@@ -23,19 +23,19 @@ curl -X POST http://localhost:8080/users/1/favorites \
           "birthCountry": "USA",
           "hoursSpentOnMedia": 4,
           "numberOfPurchases": 10
-     }'
+         }'
 
-# ADD existing user favorite
+# ADD user favorite which already exists
 curl -X POST http://localhost:8080/users/1/favorites \
      -H "Content-Type: application/json" \
      -d '{
-          "id": 1,
+          "id": 2,
           "type": "Insight",
           "description": "Sample Insight for testing",
           "text": "Testing Insight"
-        }'
+         }'
 
-# ADD invalid user favorite
+# ADD user favorite with invalid type
 curl -X POST http://localhost:8080/users/1/favorites \
      -H "Content-Type: application/json" \
      -d '{
@@ -43,24 +43,51 @@ curl -X POST http://localhost:8080/users/1/favorites \
           "type": "INVALIDTYPE",
           "description": "Sample Insight for testing",
           "text": "Testing Insight"
-          }'
+         }'
 
-# EDIT valid user favorite
-curl -X PUT http://localhost:8080/users/1/favorites/1 \
+# EDIT the previously added user favorite
+curl -X PUT http://localhost:8080/users/1/favorites/100 \
      -H "Content-Type: application/json" \
      -d '{
-          "id": 1,
-          "type": "Insight",
-          "description": "Updated Insight for testing",
-          "text": "Updated Insight"
-     }'
+          "id": 100,
+          "type": "Audience",
+          "description": "Updated Audience",
+          "age": 18,
+          "ageGroup": "18-25",
+          "gender": "Female",
+          "birthCountry": "Greece",
+          "hoursSpentOnMedia": 15,
+          "numberOfPurchases": 25
+         }'
 
-# EDIT user favorite with mismatched id
-curl -X PUT http://localhost:8080/users/1/favorites/10 \
+# EDIT user favorite with mismatched id (assetID in URL and id in body)
+curl -X PUT http://localhost:8080/users/2/favorites/2 \
      -H "Content-Type: application/json" \
      -d '{
           "id": 1,
           "type": "Insight",
           "description": "Sample Insight for testing",
           "text": "Testing Insight"
-     }'
+         }'
+
+# EDIT user favorite with mismatched type
+curl -X PUT http://localhost:8080/users/1/favorites/100 \
+     -H "Content-Type: application/json" \
+     -d '{
+          "id": 100,
+          "type": "Chart",
+          "description": "This is a chart",
+          "title": "Chart",
+          "xAxesTitle": "X-Axis",
+          "yAxesTitle": "Y-Axis",
+          "dataPoints": [
+          {
+               "X": 10,
+               "Y": 10
+          },
+          {
+               "X": 20,
+               "Y": 20
+          }
+          ]
+        }'
